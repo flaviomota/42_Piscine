b2dff6b032cc9107092aa9aacfa4d5e9b4fbf830
Flávio Mota <70024603+flaviomota@users.noreply.github.com>
2021-05-14 12:58:04 +0100

    Shell00_01

---
+++ Shell/Shell_01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -v -e "#" | awk "NR % 2 == 0" | cut -d':' -f1 | rev | sort -r | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2' | sed 's/$/,/' | tr '\n' ' ' | sed 's/, *$/\./' | tr -d '\n'
